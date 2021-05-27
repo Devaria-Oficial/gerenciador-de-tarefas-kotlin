f6a910fd5e092d8358b04164690c48760dabfb75
Rafael Thomazelli <rafael@verzel.com.br>
2021-05-26 22:28:54 -0300

    Update LoginController.kt

--- src/main/kotlin/br/com/devaria/projetos/gerenciadortarefas/controllers/LoginController.kt
+++ src/main/kotlin/br/com/devaria/projetos/gerenciadortarefas/controllers/LoginController.kt
@@ -17,7 +17,13 @@ class LoginController {
     @PostMapping
     fun efetuarLogin(@RequestBody dto : LoginDTO) : ResponseEntity<Any>{
         try{
-            throw RuntimeException("Testando uma exceção")
+            if(dto == null || dto.login.isNullOrBlank() || dto.login.isNullOrEmpty()
+                || dto.senha.isNullOrEmpty() || dto.senha.isNullOrBlank()){
+                return ResponseEntity(ErroDTO(HttpStatus.BAD_REQUEST.value(),
+                    "Parâmetros de entrada inválidos"), HttpStatus.BAD_REQUEST)
+            }
+
+            return ResponseEntity("Login autenticado com sucesso", HttpStatus.OK)
         }catch (e: Exception){
             return ResponseEntity(ErroDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 "Não foi possível efetuar o login, tente novamente"), HttpStatus.INTERNAL_SERVER_ERROR)
